c53a037e8a7f68f5465a9b00d36f9a1d4ba0420b
chr1swallace <chris.wallace@cimr.cam.ac.uk>
2013-07-03 11:21:07 +0100

    roxygenize

--- R/wilcoxon.R
+++ R/wilcoxon.R
@@ -14,6 +14,47 @@ function(p,snps.in,n0,w=NULL) {
   return(sum(R) - n0 * (n0+1)/2)
 }
 
+
+
+
+
+##' Wilcoxon test statistic, with optional weights.
+##' 
+##' Calculate a Wilcoxon two group rank test statistic, with optional
+##' propensity score weighting.
+##' 
+##' 
+##' @param p a numeric vector of observed p values from a list of SNPs or a
+##' matrix, with each column representing a vector under a different
+##' permutation of the dataset.  
+##' @param snps.in a numeric vector indicating which members of p form the test
+##' group (their complement form the control group). 
+##' @param weights optional propensity score weights.  These are binned
+##' according to binsize, and a weight calculated which is inversely
+##' proportional to the probability of sampling a member of the test group in
+##' that bin. 
+##' @param binsize see weights, above.
+##' @return A numeric value or, if p is a matrix, a numeric vector.
+##' @author Chris Wallace
+##' @export
+##' @seealso \code{\link{Z.value}}
+##' @references Propensity weights are described
+##' 
+##' Rosenbaum, P. R. & Rubin, D. B. The central role of the propensity score in
+##' observational studies for causal effects. Biometrika, 1983, 70, 41-55
+##' 
+##' Rosenbaum, P. R. Model-based direct adjustment. Journal of the American
+##' Statistical Association, 1987, 82, 387-394
+##' @keywords htest
+##' @examples
+##' 
+##' x <- exp(-rexp(1000)) # uniform
+##' y <- exp(-rexp(1000,0.8)) # skewed towards 0
+##' wilcoxon(p=c(x,y),snps.in=1:1000)
+##' 
+##' ## note, should be equal to
+##' wilcox.test(x,y)
+##' 
 wilcoxon <-
 function(p,snps.in,weights=NULL,binsize=0.05) {
   n0 <- length(snps.in)
